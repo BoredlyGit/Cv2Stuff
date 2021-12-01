2ced6d641cda55f8ef262509afec3159ce660d64
Bad-At-Python <68675343+Bad-At-Python@users.noreply.github.com>
2021-11-30 23:41:54 -0500

    started custom ball detection

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import cv2
 import numpy
 import sys
 
-img = cv2.imread("images/ball.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+og_img = cv2.imread("images/test_1.jpg")
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+img = cv2.GaussianBlur(og_img, (7, 7), 0)
 
-cv2.imshow("test", img)
+img = cv2.inRange(img, (1, 110, 110), (70, 220, 240))
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+print(contours)
+# TODO: Filter contours by shape
+cv2.drawContours(og_img, contours, -1, (0, 255, 0), 2)
+
+cv2.imshow("processed", img)
+
+cv2.imshow("og", og_img)
 cv2.waitKey(0)
