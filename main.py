db383ded22bc7284a344e31caf4668e5a0b8a345
Bad-At-Python <68675343+Bad-At-Python@users.noreply.github.com>
2021-12-05 18:56:49 -0500

    Basic algorithim complete, needs fine-tuning. Works on test 1 (2 kinda) but cv2.inRange ranges need improvement (esp. for tests 3 & 4)

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 import cv2
 import numpy
-import sys
+import numpy as np
 
-og_img = cv2.imread("images/test_1.jpg")
+all_contours = False
 
-img = cv2.GaussianBlur(og_img, (7, 7), 0)
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) No grayscale cause using color masks
+og_img = cv2.imread("images/test_3.jpg")
+img = cv2.GaussianBlur(og_img, (3, 3), 0)
+# No grayscale cause using color masks
 
-# SUGGESTION: take 2 ranges, 1 for the lit sections of the ball, and one for the shadowed section and combine?
+# TODO: improve ranges to accommodate tests 3 & 4 (seems like upper limit needs increasing) (maybe hsv)
 lit_ball_mask = cv2.inRange(img, (1, 110, 110), (70, 220, 240))
 shadowed_ball_mask = cv2.inRange(img, (0, 50, 50), (25, 130, 130))
 mask = lit_ball_mask + shadowed_ball_mask
+
+# try to detect edges of overlapping balls
+ball_edges = cv2.GaussianBlur(cv2.Canny(img, 0, 250), (3, 3), 0)
+ball_edges = numpy.logical_and(mask, ball_edges)
+ball_edges.dtype = np.dtype("uint8")
+ball_edges = ball_edges * 255
+mask = mask - ball_edges
+
+cv2.imshow("mask", mask)
+cv2.imshow("ball_edges", ball_edges)
+
 contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# print(contours)
 
-print(contours)
-# TODO: Filter contours by shape
-# TODO: Account for balls next to each other appearing as one contour
+for contour in contours:
+    print(3.14 * cv2.minEnclosingCircle(contour)[1] ** 2, cv2.contourArea(contour), ((3.14 * cv2.minEnclosingCircle(contour)[1] ** 2)/10), (3.14 * cv2.minEnclosingCircle(contour)[1] ** 2 - cv2.contourArea(contour)))
+
+if not all_contours:
+    # check size and shape
+    contours = [contour for contour in contours if cv2.contourArea(contour) > 500 and (3.14 * cv2.minEnclosingCircle(contour)[1] ** 2 - cv2.contourArea(contour) < (3.14 * cv2.minEnclosingCircle(contour)[1] ** 2)/4) ]
+
+circles = [cv2.minEnclosingCircle(contour) for contour in contours]
+
 cv2.drawContours(img, contours, -1, (0, 255, 0), 1)
-
+for circle in circles:
+    cv2.circle(img, (int(circle[0][0]), int(circle[0][1])), int(circle[1]), (255, 0, 0), 2)
 
 cv2.imshow("processed", img)
-
-# cv2.imshow("og", og_img)
 cv2.waitKey(0)
