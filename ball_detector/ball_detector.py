7817c4e115478603e0cb83a799bc978a6a640669
Bad-At-Python <68675343+Bad-At-Python@users.noreply.github.com>
2021-12-17 20:16:39 -0500

    Full support for live video in ball detector.

--- ball_detector/ball_detector.py
+++ ball_detector/ball_detector.py
@@ -8,75 +8,88 @@ import pprint
 # TODO: try to detect blobs of balls somehow?
 # Suggestion: use ellipses instead and check ratio?
 
-img_path = "images/test_1.jpg"
-img_path = img_path.lower()
-all_contours = False
-use_hsv = True
-use_video = True
 
+def detect_balls(img, profile, all_contours=False):
+    if profile["img_resize"]:
+        img = cv2.resize(img, profile["img_resize"])
 
-with open("profiles.json") as img_profiles:
-    img_profiles = json.load(img_profiles)
-img_profile = img_profiles["default"]
-img_profile.update(img_profiles.get(img_path, {}))
-print(f"Using profile: {img_profile['name']}\n{pprint.pformat(img_profile)}")
-
-
-# img = cv2.imread(img_path)
-if use_video:
-    cap = cv2.VideoCapture(0)
-
-while True:
-    if use_video:
-        _, og_img = cap.read()
-        img = og_img.copy()
-
-        h, w = img.shape[:2]
-        center = (h//2, w//2)
-        img = cv2.warpAffine(img, cv2.getRotationMatrix2D(center, 180, 1.0), (w, h))
-        og_img = cv2.warpAffine(og_img, cv2.getRotationMatrix2D(center, 180, 1.0), (h, w))
-    else:
-        og_img = cv2.imread(img_path)
-
-    if img_profile["img_resize"]:
-        img = cv2.resize(img, img_profile["img_resize"])
     img = cv2.GaussianBlur(img, (3, 3), 0)
     # No grayscale cause using color masks
 
-    if use_hsv:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        # doesnt accept tuples for some reason.
-        mask = cv2.inRange(img, np.array(img_profile["ball_hsv_min"]), np.array(img_profile["ball_hsv_max"]))
-    else:
-        lit_ball_mask = cv2.inRange(img, (1, 110, 110), (70, 220, 240))
-        shadowed_ball_mask = cv2.inRange(img, (0, 50, 50), (25, 130, 130))
-        mask = lit_ball_mask + shadowed_ball_mask
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # doesnt accept tuples for some reason.
+    mask = cv2.inRange(img, np.array(profile["ball_hsv_min"]), np.array(profile["ball_hsv_max"]))
 
-
-    # try to detect edges of overlapping balls
+    # try to detect edges of overlapping balls with Canny - it finds edges which can be subtracted from the mask
     ball_edges = cv2.GaussianBlur(cv2.Canny(img, 0, 250), (3, 3), 0)
     ball_edges = numpy.logical_and(mask, ball_edges)
     ball_edges.dtype = np.dtype("uint8")
     ball_edges = ball_edges * 255
     mask = mask - ball_edges
 
-    # cv2.imshow("mask", mask)
-
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if not all_contours:
         # check size and shape, (see variables above)
         contours = [contour for contour in contours
-                    if cv2.contourArea(contour) > img_profile["ball_min_area"]
+                    if cv2.contourArea(contour) > profile["ball_min_area"]
                     and (3.14 * cv2.minEnclosingCircle(contour)[1] ** 2 - cv2.contourArea(contour)
-                         < (3.14 * cv2.minEnclosingCircle(contour)[1] ** 2) * (1 - img_profile["ball_min_coverage"]))]
+                         < (3.14 * cv2.minEnclosingCircle(contour)[1] ** 2) * (1 - profile["ball_min_coverage"]))]
 
-    circles = [cv2.minEnclosingCircle(contour) for contour in contours]
+    return list(map(cv2.minEnclosingCircle, contours)), contours
 
-    cv2.drawContours(og_img, contours, -1, (255, 255, 255), 2)
-    for circle in circles:
-        cv2.circle(og_img, (int(circle[0][0]), int(circle[0][1])), int(circle[1]), (255, 0, 0), 2)
 
-    cv2.imshow("", og_img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def main():
+    img_path = "images/test_1.jpg"
+    img_path = img_path.lower()
+    use_video = False
+    camera_name = ""
+    rotate_video = 0
+
+    with open("profiles.json") as img_profiles:
+        img_profiles = json.load(img_profiles)
+    img_profile = img_profiles["default"]
+
+    if use_video:
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        img_profile.update(img_profiles.get(camera_name, {}))
+        print(f"Using profile: {img_profile['name']}\n{pprint.pformat(img_profile)}")
+
+        while True:
+            frame_ready = False
+            og_img = None
+            while not frame_ready:
+                frame_ready, og_img = cap.read()
+
+            if rotate_video:
+                h, w = og_img.shape[:2]
+                print(w, h)
+                center = (w//2, h//2)
+                og_img = cv2.warpAffine(og_img, cv2.getRotationMatrix2D(center, rotate_video, 1), (w, h))
+
+            circles, contours = detect_balls(og_img.copy(), img_profile)
+            cv2.drawContours(og_img, contours, -1, (255, 255, 255), 2)
+            for circle in circles:
+                cv2.circle(og_img, (int(circle[0][0]), int(circle[0][1])), int(circle[1]), (255, 0, 0), 2)
+
+            cv2.imshow("processed", og_img)
+            key = cv2.waitKey(1)
+            if key in (27, 113):  # 27 = esc, 113 = q
+                print(key)
+                break
+    else:
+        og_img = cv2.imread(img_path)
+        img_profile.update(img_profiles.get(img_path, {}))
+        print(f"Using profile: {img_profile['name']}\n{pprint.pformat(img_profile)}")
+
+        circles, contours = detect_balls(og_img.copy(), img_profile)
+
+        cv2.drawContours(og_img, contours, -1, (255, 255, 255), 2)
+        for circle in circles:
+            cv2.circle(og_img, (int(circle[0][0]), int(circle[0][1])), int(circle[1]), (255, 0, 0), 2)
+        cv2.imshow("processed", og_img)
+        cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()
